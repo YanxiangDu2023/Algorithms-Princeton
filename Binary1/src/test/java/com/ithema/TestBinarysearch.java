b016ef6547703ae0d446383043d0365c0fa0e765
Lei Chun <136141200+YanxiangDu2023@users.noreply.github.com>
2024-09-22 00:14:45 +0200

    Add files via upload

---
+++ Binary1/src/test/java/com/ithema/TestBinarysearch.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.itheima.BinarySearch.binarySearchBasic;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestBinarySearch {
+
+    @Test
+    @DisplayName("测试 binarySearchBasic")
+    public void test1() {
+        int[] a = {7, 13, 21, 30, 38, 44, 52, 53};
+        assertEquals(0, binarySearchBasic(a, 7));
+        assertEquals(1, binarySearchBasic(a, 13));
+        assertEquals(2, binarySearchBasic(a, 21));
+        assertEquals(3, binarySearchBasic(a, 30));
+        assertEquals(4, binarySearchBasic(a, 38));
+        assertEquals(5, binarySearchBasic(a, 44));
+        assertEquals(6, binarySearchBasic(a, 52));
+        assertEquals(7, binarySearchBasic(a, 53));
+
+        assertEquals(-1, binarySearchBasic(a, 0));
+        assertEquals(-1, binarySearchBasic(a, 15));
+        assertEquals(-1, binarySearchBasic(a, 60));
+    }
+
+    @Test
+    @DisplayName("测试右移运算")
+    public void test2() {
+        int i = 0;
+        int j = Integer.MAX_VALUE - 1;
+        // 模拟第一次求中间索引
+        int m = (i + j) / 2;
+        // 模拟第二次求中间索引, target 在右侧
+        i = m + 1;
+        assertEquals(1073741824, i);
+        assertEquals(2147483646, j);
+        assertEquals(-1073741826, i + j);
+        m = (i + j) / 2; // 有问题的情况
+        assertEquals(-536870913, m);
+        m = (i + j) >>> 1; // 改正后的情况
+        assertEquals(1610612735, m);
+        /*
+            同一个二进制数
+            1011_1111_1111_1111_1111_1111_1111_1110
+
+            不把最高位视为符号位, 代表 3221225470
+            把最高位视为符号位, 代表 -1073741826
+         */
+    }
+}
